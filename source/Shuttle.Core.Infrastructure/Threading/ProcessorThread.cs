f43f58c933e49846323e98081cf995a852ed008a
Eben <me@ebenroux.co.za>
2014-01-27 21:49:40 +0200

    - added Thread.Join to ProcessorThread

--- source/Shuttle.Core.Infrastructure/Threading/ProcessorThread.cs
+++ source/Shuttle.Core.Infrastructure/Threading/ProcessorThread.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading;
 
 namespace Shuttle.Core.Infrastructure
@@ -7,7 +6,9 @@ namespace Shuttle.Core.Infrastructure
 	{
 		private readonly IProcessor processor;
 		private volatile bool active;
-        private int threadJoinTimeoutInterval = ConfigurationItem<int>.ReadSetting("ThreadJoinTimeoutInterval", 200).GetValue();
+
+		private readonly int threadJoinTimeoutInterval =
+			ConfigurationItem<int>.ReadSetting("ThreadJoinTimeoutInterval", 1000).GetValue();
 
 		private Thread thread;
 
@@ -31,47 +32,52 @@ namespace Shuttle.Core.Infrastructure
 
 			thread.SetApartmentState(ApartmentState.MTA);
 			thread.IsBackground = true;
-		    thread.Priority = ThreadPriority.Normal;
+			thread.Priority = ThreadPriority.Normal;
 
 			active = true;
 
 			thread.Start();
 
-            log.Trace(string.Format(InfrastructureResources.TraceProcessorThreadStarting, thread.ManagedThreadId, processor.GetType().FullName));
-            
-            while (!thread.IsAlive && active)
+			log.Trace(string.Format(InfrastructureResources.TraceProcessorThreadStarting, thread.ManagedThreadId,
+			                        processor.GetType().FullName));
+
+			while (!thread.IsAlive && active)
 			{
 			}
 
-            if (active)
-            {
-                log.Trace(string.Format(InfrastructureResources.TraceProcessorThreadActive, thread.ManagedThreadId, processor.GetType().FullName));
-            }
+			if (active)
+			{
+				log.Trace(string.Format(InfrastructureResources.TraceProcessorThreadActive, thread.ManagedThreadId,
+				                        processor.GetType().FullName));
+			}
 		}
 
-	    public void Stop()
+		public void Stop()
 		{
-			if (!active)
-			{
-				return;
-			}
-
-			log.Trace(string.Format(InfrastructureResources.TraceProcessorThreadStopping, thread.ManagedThreadId, processor.GetType().FullName));
+			log.Trace(string.Format(InfrastructureResources.TraceProcessorThreadStopping, thread.ManagedThreadId,
+			                        processor.GetType().FullName));
 
 			active = false;
+
+			if (thread.IsAlive)
+			{
+				thread.Join(threadJoinTimeoutInterval);
+			}
 		}
 
 		private void Work()
 		{
 			while (active)
 			{
-				log.Verbose(string.Format(InfrastructureResources.VerboseProcessorExecuting, thread.ManagedThreadId, processor.GetType().FullName));
+				log.Verbose(string.Format(InfrastructureResources.VerboseProcessorExecuting, thread.ManagedThreadId,
+				                          processor.GetType().FullName));
 
 				processor.Execute(this);
-            }
+			}
 
-            log.Trace(string.Format(InfrastructureResources.TraceProcessorThreadStopped, thread.ManagedThreadId, processor.GetType().FullName));
-        }
+			log.Trace(string.Format(InfrastructureResources.TraceProcessorThreadStopped, thread.ManagedThreadId,
+			                        processor.GetType().FullName));
+		}
 
 		public bool Active
 		{
--- source/Shuttle.Core.Infrastructure/Threading/ProcessorThreadPool.cs
+++ source/Shuttle.Core.Infrastructure/Threading/ProcessorThreadPool.cs
@@ -3,97 +3,97 @@ using System.Collections.Generic;
 
 namespace Shuttle.Core.Infrastructure
 {
-    public class ProcessorThreadPool : IProcessorThreadPool
-    {
-        private readonly string name;
-        private readonly IProcessorFactory processorFactory;
-        private readonly int threadCount;
-        private readonly List<ProcessorThread> threads = new List<ProcessorThread>();
-        private bool disposed;
-        private bool started;
+	public class ProcessorThreadPool : IProcessorThreadPool
+	{
+		private readonly string name;
+		private readonly IProcessorFactory processorFactory;
+		private readonly int threadCount;
+		private readonly List<ProcessorThread> threads = new List<ProcessorThread>();
+		private bool disposed;
+		private bool started;
 
-        public ProcessorThreadPool(string name, int threadCount, IProcessorFactory processorFactory)
-        {
-            this.name = name;
-            this.threadCount = threadCount;
-            this.processorFactory = processorFactory;
-        }
+		public ProcessorThreadPool(string name, int threadCount, IProcessorFactory processorFactory)
+		{
+			this.name = name;
+			this.threadCount = threadCount;
+			this.processorFactory = processorFactory;
+		}
 
-        public void Pause()
-        {
-            foreach (var thread in threads)
-            {
-                thread.Stop();
-            }
+		public void Pause()
+		{
+			foreach (var thread in threads)
+			{
+				thread.Stop();
+			}
 
-            Log.For(this).Information(string.Format(InfrastructureResources.TaskPoolStatusChange, name, "paused"));
-        }
+			Log.For(this).Information(string.Format(InfrastructureResources.TaskPoolStatusChange, name, "paused"));
+		}
 
-        public void Resume()
-        {
-            foreach (var thread in threads)
-            {
-                thread.Start();
-            }
+		public void Resume()
+		{
+			foreach (var thread in threads)
+			{
+				thread.Start();
+			}
 
-            Log.For(this).Information(string.Format(InfrastructureResources.TaskPoolStatusChange, name, "resumed"));
-        }
+			Log.For(this).Information(string.Format(InfrastructureResources.TaskPoolStatusChange, name, "resumed"));
+		}
 
-        public IProcessorThreadPool Start()
-        {
-            if (started)
-            {
-                return this;
-            }
+		public IProcessorThreadPool Start()
+		{
+			if (started)
+			{
+				return this;
+			}
 
-            if (threadCount < 1)
-            {
-                throw new ThreadCountZeroException();
-            }
+			if (threadCount < 1)
+			{
+				throw new ThreadCountZeroException();
+			}
 
-            StartThreads();
+			StartThreads();
 
-            started = true;
+			started = true;
 
-            Log.For(this).Information(string.Format(InfrastructureResources.TaskPoolStatusChange, name, "started"));
+			Log.For(this).Information(string.Format(InfrastructureResources.TaskPoolStatusChange, name, "started"));
 
-            return this;
-        }
+			return this;
+		}
 
-        public void Dispose()
-        {
-            Dispose(true);
+		public void Dispose()
+		{
+			Dispose(true);
 
-            GC.SuppressFinalize(this);
-        }
+			GC.SuppressFinalize(this);
+		}
 
-        private void StartThreads()
-        {
-            var i = 0;
+		private void StartThreads()
+		{
+			var i = 0;
 
-            while (i++ < threadCount)
-            {
-                var thread = new ProcessorThread(processorFactory.Create());
+			while (i++ < threadCount)
+			{
+				var thread = new ProcessorThread(processorFactory.Create());
 
-                threads.Add(thread);
+				threads.Add(thread);
 
-                thread.Start();
-            }
-        }
+				thread.Start();
+			}
+		}
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposed)
-            {
-                return;
-            }
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposed)
+			{
+				return;
+			}
 
-            if (disposing)
-            {
-                foreach (var thread in threads)
-                {
-                    thread.Deactivate();
-                }
+			if (disposing)
+			{
+				foreach (var thread in threads)
+				{
+					thread.Deactivate();
+				}
 
 				foreach (var thread in threads)
 				{
@@ -101,7 +101,7 @@ namespace Shuttle.Core.Infrastructure
 				}
 			}
 
-            disposed = true;
-        }
-    }
+			disposed = true;
+		}
+	}
 }
