ad10ac30d7b87f0c2f29806897364da963e44010
Eben <me@ebenroux.co.za>
2014-04-22 19:33:51 +0200

    - removed superfluous castle projects

--- source/Shuttle.Core.Data.Castle/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Shuttle.Core.Data.Castle")]
-[assembly: AssemblyVersion("3.0.1.0")]
--- source/Shuttle.Core.Infrastructure.Castle/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Shuttle.Core.Infrastructure.Castle")]
-[assembly: AssemblyVersion("3.0.1.0")]
