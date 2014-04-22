6926508ae4159cc1b40aacfc201a30afad0e0bff
Eben <me@ebenroux.co.za>
2014-04-22 20:22:37 +0200

    - v3.0.2 release

--- source/Shuttle.Core.Infrastructure/Properties/AssemblyInfo.cs
+++ source/Shuttle.Core.Infrastructure/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Shuttle.Core.Infrastructure")]
-[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
 [assembly: InternalsVisibleTo("Test.All")]
