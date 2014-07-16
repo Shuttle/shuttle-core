0eb5c96e83f083d3a015e1900169a395493771f1
Eben <me@ebenroux.co.za>
2014-07-16 12:21:21 +0200

    - published v3.1.2 of generic host

--- source/Shuttle.Core.Infrastructure/Properties/AssemblyInfo.cs
+++ source/Shuttle.Core.Infrastructure/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Shuttle.Core.Infrastructure")]
-[assembly: AssemblyVersion("3.1.1.0")]
+[assembly: AssemblyVersion("3.1.2.0")]
 [assembly: InternalsVisibleTo("Test.All")]
