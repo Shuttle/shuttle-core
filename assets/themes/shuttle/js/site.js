4fd55e85fbcdd9a78c25dc247493f94cd261d308
eben-roux <me@ebenroux.co.za>
2014-07-22 08:13:59 +0200

    - fixed scrollTop / TOC

--- assets/themes/shuttle/js/site.js
+++ assets/themes/shuttle/js/site.js
@@ -1,12 +1,30 @@
 $(function(){
+	var hasTOC = $("#toc").length > 0;
+	
 	var setTOC = function() {
+		var toc = $("#toc");
+		
+		if (!hasTOC){
+			return;
+		}
+	
 		$("#toc").width($('#toc-container').width())
 	};
 	
     var move = function() {
-        var st = $(window).scrollTop();
-        var ot = $("#toc-anchor").offset().top;
-        var s = $("#toc");
+		var st = $(window).scrollTop();
+		var ot;
+		var s;
+	
+		$('#footer').css('position', (st + $(window).height()) < ($(document).height() - $(window).height()) ? 'inherit' : 'fixed');
+
+		if (!hasTOC) {
+			return;
+		}
+		
+        ot = $("#toc-anchor").offset().top;
+        s = $("#toc");
+		
         if(st > ot) {
             s.css({
                 position: "fixed",
@@ -20,23 +38,22 @@ $(function(){
                 });
             }
         }
-		
-		$('#footer').css('position', (st + $(window).height()) < ($(document).height() - $(window).height()) ? 'inherit' : 'fixed');
     };
 	
 	setTOC();
 
 	$(window).resize(setTOC);
 
-	$("#toc")
-		.tocify({
-			theme: 'bootstrap'
-		})
-		.data('toc-tocify');
+	if (hasTOC) {
+		$("#toc")
+			.tocify({
+				theme: 'bootstrap'
+			})
+			.data('toc-tocify');
+	}
 	
 	$('table').addClass('table table-hover table-condensed table-responsive');
 	
     $(window).scroll(move);
-
     move();	
 })
