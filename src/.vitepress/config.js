13fced7db352f26890ef60bf14ef88e0dd7403e0
Eben <me@ebenroux.co.za>
2022-03-21 15:34:33 +0200

    - algolia docsearch

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -234,11 +234,11 @@ module.exports = (async () => {
         ],
 
         themeConfig: {
-            // algolia: {
-            //     indexName: 'Shuttle.Core Documentation',
-            //     appId: '42GX712C20',
-            //     apiKey: 'cc059819a5d38ee7a55298f7cf9ed70a'
-            // },
+            algolia: {
+                indexName: 'shuttle',
+                appId: '8AVNJSMBZI',
+                apiKey: 'a17fbccfc1eca711e2ec7712bfcec46f'
+            },
 
             // carbonAds: {
             //     code: '',
