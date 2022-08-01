2cd782f66f1cf201dc68bf1847a865e52b27be68
Eben <me@ebenroux.co.za>
2022-08-01 07:01:12 +0200

    deploy

---
+++ assets/infrastructure_shuttle-core-contract.md.85d202eb.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.28ec495d.js";
+const __pageData = '{"title":"Shuttle.Core.Contract","description":"","frontmatter":{},"headers":[],"relativePath":"infrastructure/shuttle-core-contract.md"}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 21);
+const _hoisted_22 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_22);
+}
+var shuttleCoreContract = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, shuttleCoreContract as default };
