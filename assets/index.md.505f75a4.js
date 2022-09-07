3bece4652e93ef38851a1574915b6c0c8ac1b8a4
Eben <me@ebenroux.co.za>
2022-09-07 15:30:59 +0200

    deploy

---
+++ assets/index.md.505f75a4.js
@@ -0,0 +1,26 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, d as createStaticVNode, e as createVNode } from "./app.cfdf8867.js";
+const Home_vue_vue_type_style_index_0_scoped_b139ba49_lang = "";
+const _sfc_main$1 = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<section id="hero" data-v-b139ba49><p class="upgrade-notice" data-v-b139ba49> The latest version of the Shuttle.Core components now make use of <a href="https://docs.microsoft.com/en-us/dotnet/core/extensions/dependency-injection" target="_blank" data-v-b139ba49>.NET dependency injection</a> as well as the <a href="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/options?view=aspnetcore-6.0" target="_blank" data-v-b139ba49>options pattern</a>. This is a major departure from the component container adapter pattern used to date. Please take a look at the <a href="/shuttle-core/upgrade-ms-di.html" data-v-b139ba49>upgrade guide</a> to see the <strong data-v-b139ba49>breaking changes</strong>. </p><h1 class="tagline" data-v-b139ba49>Useful .Net stuff</h1><p class="description" data-v-b139ba49> Cross-cutting packages that are used to facilitate the development of .Net software. </p></section><section id="highlights" class="vt-box-container" data-v-b139ba49><div class="vt-box" data-v-b139ba49><h2 data-v-b139ba49>Framework Support</h2><div data-v-b139ba49>Packages currently target <code data-v-b139ba49>netstandard2.0</code> and <code data-v-b139ba49>netstandard2.1</code> which means that they can be used with .NET Core 2.1+, .NET Framework 4.6.1+, and .NET 5.0+</div></div><div class="vt-box" data-v-b139ba49><h2 data-v-b139ba49>Common Patterns</h2><div data-v-b139ba49>Many common patterns and mechanisms are provided that get you up-and-running quickly.</div></div><div class="vt-box" data-v-b139ba49><h2 data-v-b139ba49>Open Source</h2><div data-v-b139ba49>These packages are free open source software licensed under the <a href="https://opensource.org/licenses/BSD-3-Clause" data-v-b139ba49>3-Clause BSD License</a>. Pull requests are welcome.</div></div></section>', 2);
+const _hoisted_3 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, _hoisted_3);
+}
+const Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-b139ba49"]]);
+const __pageData = JSON.parse('{"title":"Home","description":"","frontmatter":{"page":true,"title":"Home"},"headers":[],"relativePath":"index.md"}');
+const __default__ = { name: "index.md" };
+const _sfc_main = Object.assign(__default__, {
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", null, [
+        createVNode(Home)
+      ]);
+    };
+  }
+});
+export {
+  __pageData,
+  _sfc_main as default
+};
